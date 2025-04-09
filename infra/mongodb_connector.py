a8f334d013f93e00fc3480eb1bc9d528614cb617
Manaswini Gogineni <goginenim18069@gmail.com>
2025-04-09 01:02:39 -0500

    db connection to docker container

---
+++ infra/mongodb_connector.py
@@ -0,0 +1,119 @@
+import os
+import logging
+from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure, ServerSelectionTimeoutError
+
+# adding logging formatting
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+MONGO_URI = os.getenv('MONGO_URI', 'mongodb://localhost:27017/')
+MONGO_DB_NAME = os.getenv('MONGO_DB_NAmE', 'data_ingestion_db')
+
+mongo_client = None
+
+'''
+Function is used to connect the the MONGO_URI server running
+For the application it is the mongo server running in the docker container
+The function will check for an existing connection and will reuse if there is a connection
+Otherwise, it will try to establish a new connection
+Function will return None if there is an exception or if the connection timed out in 5000ms
+'''
+def get_mongo_client():
+    global mongo_client
+    if mongo_client:
+        try:
+            mongo_client.admin.command('ping')
+            logging.debug("Reusing existing connection")
+            return mongo_client
+        except (ConnectionFailure, ServerSelectionTimeoutError):
+            logging.warning("Exiting MongoDB connection lost, trying to reconnect!")
+            mongo_client = None
+    try:
+        logging.info(f"Trying to connect to the MongoDB at {MONGO_URI}")
+        client = MongoClient(MONGO_URI, serverSelectionTimeoutMS=5000)
+        client.admin.command('ismaster')
+        mongo_client = client
+        logging.info(f"Successfully connected to MongoDd: {MONGO_URI}")
+        return mongo_client
+    except (ConnectionFailure, ServerSelectionTimeoutError) as e:
+        logging.error(f"Failed to connect to MongoServer {MONGO_URI}: {e}")
+        return None
+    except Exception as e:
+        logging.error(f"Failed to connect to the MongoServer {MONGO_URI}: {e}")
+        return None
+
+'''
+The function get the mention Mongo URI client and get the mentioned Mongo Db from the server
+If any error during the process logs the exception
+'''
+def get_db():
+    client = get_mongo_client()
+    if client:
+        try:
+            db = client[MONGO_DB_NAME]
+            return db
+        except Exception as e:
+            logging.error(f"Failed to get database '{MONGO_DB_NAME}': '{e}'")
+            return None
+    else:
+        logging.error("Cannot get database as MongoDB client is not available")
+        return None
+
+'''
+Inserts the job dictionary into the specified collection
+If successful return the result from pymongo.result.InsertOneResult else None
+'''
+def insert_item(item: dict, collection_name: str):
+    db = get_db()
+    if db:
+        try:
+            collection = db[collection_name]
+            result = collection.insert_one(item)
+            logging.info(f"Inserted item with id {result.inserted_id} into {collection_name}")
+            return result
+        except Exception as e:
+            logging.error(f"Failed to insert item into {collection_name}: {e}")
+            return None
+    return None
+
+'''
+Get the db from the server
+Runs the query against the collection
+return query response, if exception return empty list
+'''
+def find_item(query: dict, collection_name: str, projection: dict = None):
+    db = get_db()
+    items = []
+    if db:
+        try:
+            collection = db[collection_name]
+            cursor = collection.find(query, projection)
+            items = list(cursor)
+            logging.debug(f"Found {len(items)} items in {collection_name} matching query")
+        except Exception as e:
+            logging.error(f"Failed to fin items in {collection_name}: {e}")
+            return []
+    return items
+
+'''
+Close the mongo server connection
+'''
+def close_mongo_connection():
+    global mongo_client
+    if mongo_client:
+        mongo_client.close()
+        mongo_client = None
+        logging.info("MongoDB connection closed")
+
+'''
+Runs a sample test connection to the mongo server when the python file is directly ran
+'''
+if __name__ == "__main__":
+    logging.info("Testing MongoDB connection")
+    db_instance = get_db()
+
+    if db_instance is not None:
+        logging.info(f"Successfully obtained database instance: {db_instance.name}")
+    else:
+        logging.error("Failed to get database instance.")
+    close_mongo_connection()
---
+++ infra/redis_connector.py
@@ -0,0 +1,117 @@
+import os
+import logging
+import redis
+from redis.exceptions import ConnectionError
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+REDIS_URL = os.getenv('REDIS_URL', 'redis://localhost:6379/0')
+
+redis_pool = None
+
+'''
+Creates a redis connection pool
+If exception occurs between connection loggs the error
+Return the redis pool connection
+'''
+def get_redis_pool():
+    global redis_pool
+    if redis_pool is None:
+        try:
+            logging.info(f"Trying to create a Redis Connection pool for the URL: {REDIS_URL}")
+            redis_pool = redis.ConnectionPool.from_url(REDIS_URL, decode_responses=True)
+            temp_client = redis.Redis(connection_pool=redis_pool)
+            temp_client.ping()
+            logging.info("Redis connection created")
+        except ConnectionError as e:
+            logging.error(f"Failed to create a Redis connection pool: {e}")
+            redis_pool = None
+        except Exception as e:
+            logging.error(f"Unexpected error while connecting to Redis: {e}")
+    return redis_pool
+
+'''
+returns the redis connection
+'''
+def get_redis_connection():
+    pool = get_redis_pool()
+    if pool:
+        try:
+            r = redis.Redis(connection_pool=pool)
+            r.ping()
+            return r
+        except ConnectionError as e:
+            logging.error(f"Error getting redis connection: {e}")
+            return None
+    else:
+        logging.error("Cannot get Redis connection, pool is not available")
+        return None
+
+'''
+Sets the key-value pair in redis
+Return True if the key and value added to redis else false
+'''
+def set_value(key: str, value: str, expire_seconds: int = None):
+    r = get_redis_connection()
+    if r:
+        try:
+            r.set(key, value, ex=expire_seconds)
+            logging.info(f"Set redis key '{key}' and it expires in {expire_seconds}")
+            return True
+        except Exception as s:
+            logging.error(f"Failed to set Redis key {key}")
+    return False
+
+'''
+Gets the value for the key from redis cache
+if key doesn't exist return None
+'''
+def get_value(key: str):
+    r = get_redis_connection()
+    value = None
+    if r:
+        try:
+            value = r.get(key)
+            logging.info(f"Got the value for the key: {key}")
+        except Exception as e:
+            logging.error(f"Got an error when getting key: {key}")
+    return value
+
+'''
+Added the value to set if not already present
+return the status of adding the value
+Logs error if any
+'''
+def add_to_set(set_name: str, value: str):
+    r = get_redis_connection()
+    added = False
+    if r:
+        try:
+            result = r.sadd(set_name, value)
+            added = (result == 1)
+            logging.info(f"Value '{value}' {'added to' if added else 'already exists in'} Redis set '{set_name}'")
+        except Exception as e:
+            logging.error(f"Failed to add the {value} because of exception: {e}")
+    return added
+
+'''
+
+'''
+def is_member(set_name: str, value: str):
+    r = get_redis_connection()
+    member = False
+    if r:
+        try:
+            member = r.sismember(set_name, value)
+            logging.info(f"Checked membership for {value} in {set_name}: {member}")
+        except Exception as e:
+            logging.error(f"Received an error while checking the member if {value} in {set_name}: {e}")
+    return member
+
+if __name__ == "__main__":
+    logging.info("Testing Redis Connection")
+    redis_conn = get_redis_connection()
+    if redis_conn:
+        logging.info("Successfully obtained the redis connection")
+    else:
+        logging.error("Failed to connect to Redis")
